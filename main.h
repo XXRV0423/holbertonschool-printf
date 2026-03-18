39070923c764960ed3edc43be03260df68023a22
XXRV0423 <xxavierrv@gmail.com>
2026-03-18 10:53:32 -0400

    All prototypes for te printf function added

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+int _putchar(char c);
+int handle_specifier(char spec, va_list args);
+
+#endif /*MAIN_H*/
